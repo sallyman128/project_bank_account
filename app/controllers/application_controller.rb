cf48c4e31ae8719397035ba9d0497d03c9727783
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-22 23:30:54 -0400

    ensure app_controller works with views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+  set :views, Proc.new { File.join(root, "../views/") }
 
   get "/" do
     erb :welcome
